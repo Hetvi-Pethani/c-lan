dc28ffaa704f4291f833e718e60ab45754be2ce2
Hetvi-Pethani <162542939+Hetvi-Pethani@users.noreply.github.com>
2024-05-21 11:32:53 +0530

    Update p1.c

--- String built-in functions/p1.c
+++ String built-in functions/p1.c
@@ -12,16 +12,16 @@ main(){
 	
 	for(i=0;i<length;i++){
 		
-		if(pwd[i]<=65 && pwd[i]>=90)upr++;
-        if(pwd[i]<=97 && pwd[i]>=122)lwr++;
-		if(pwd[i]<=48 && pwd[i]>=57)num++;		
-		if(pwd[i]=='@'|| pwd[i]=='#')spe++;
+		if(pwd[i]>=65 && pwd[i]<=90)upr++;
+        else if(pwd[i]>=97 && pwd[i]<=122)lwr++;
+		else if(pwd[i]>=48 && pwd[i]<=57)num++;		
+		else spe++;
 	}
-		if(upr>0 &&lwr>0 && num>0 &&spe>0)
+		if(upr>0 && lwr>0 && num>0 && spe>0)
 		{
-			printf("your password in weak");
+			printf("your password in Strong");
 		}
 		else{
-			printf("your password in storng");
+			printf("your password in Week");
 		}
 }
